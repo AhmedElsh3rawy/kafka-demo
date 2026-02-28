6da7ec8c0eaf22be4e82a528532028ef7530eba3
Ahmed <ahmedelsharawy117@gmail.com>
2026-03-01 00:37:47 +0200

    feat: ingest consumed user to db

--- consumer.py
+++ consumer.py
@@ -1,8 +1,16 @@
 import json
+import psycopg2
 from confluent_kafka import Consumer
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroDeserializer
 from confluent_kafka.serialization import MessageField, SerializationContext
+
+DB_CONF = {
+    "host": "localhost",
+    "database": "user_db",
+    "user": "postgres",
+    "password": "password",
+}
 
 
 def dict_to_user(obj, ctx):
@@ -29,6 +37,31 @@ consumer.subscribe(["users"])
 
 print("🟢 Consumer is running and subscribed to users topic")
 
+
+def ingest_to_db(user_data):
+    try:
+        with psycopg2.connect(**DB_CONF) as conn:
+            with conn.cursor() as cur:
+                insert_query = """
+                INSERT INTO users (id, name, email, country, created_at)
+                VALUES (%s, %s, %s, %s, %s)
+                ON CONFLICT (id) DO NOTHING;
+                """
+                cur.execute(
+                    insert_query,
+                    (
+                        user_data.get("id"),
+                        user_data.get("name"),
+                        user_data.get("email"),
+                        user_data.get("country"),
+                        user_data.get("created_at"),
+                    ),
+                )
+                conn.commit()
+    except Exception as e:
+        print(f"❌ DB Error: {e}")
+
+
 try:
     while True:
         msg = consumer.poll(1.0)
@@ -44,8 +77,9 @@ try:
             msg.value(), SerializationContext(msg.topic(), MessageField.VALUE)
         )
 
-        j = json.dumps(user, indent=4)
-        print(j)
+        if user:
+            ingest_to_db(user)
+            print(f"✅ Ingested user: {user.get('name')} ({user.get('id')})")
 except KeyboardInterrupt:
     print("\n🔴 Stopping consumer")
 
