a554ccd4823ac965422ffe50a8fb0695e8c465f7
Ahmed <ahmedelsharawy117@gmail.com>
2026-02-28 23:37:14 +0200

    feat: implement avro producer with schema registry integration

--- producer.py
+++ producer.py
@@ -1,6 +1,13 @@
 from faker import Faker
-import uuid, json, time
+import uuid, time
 from confluent_kafka import Producer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import (
+    MessageField,
+    SerializationContext,
+    StringSerializer,
+)
 
 fake = Faker()
 
@@ -9,14 +16,14 @@ producer_conf = {
     "bootstrap.servers": "localhost:9092",
 }
 
-producer = Producer(producer_conf)
+producer = Producer(dict(producer_conf))
 
 
 def delivery_report(err, msg):
     if err:
         print(f"❌ Delivery failed: {err}")
     else:
-        print(f"✅ Delivered {msg.value().decode('utf-8')}")
+        print(f"✅ Delivered to {msg.topic()} [Partition: {msg.partition()}]")
 
 
 def generate_user():
@@ -29,9 +36,39 @@ def generate_user():
     }
 
 
+def user_to_dict(user, ctx):
+    return {
+        "id": user["id"],
+        "name": user["name"],
+        "email": user["email"],
+        "country": user["country"],
+        "created_at": user["created_at"],
+    }
+
+
+with open("user.avsc", "r") as f:
+    schema_str = f.read()
+
+
+schema_registry_conf = {"url": "http://localhost:8081"}
+
+schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+avro_serializer = AvroSerializer(
+    schema_registry_client,
+    schema_str,
+    user_to_dict,
+)
+
+string_serializer = StringSerializer("utf-8")
+
 for i in range(5):
-    user = json.dumps(generate_user()).encode("utf-8")
-    producer.produce(topic="users", value=user, callback=delivery_report)
+    user = generate_user()
+    producer.produce(
+        topic="users",
+        key=string_serializer(str(i), SerializationContext("users", MessageField.KEY)),
+        value=avro_serializer(user, SerializationContext("users", MessageField.VALUE)),
+        callback=delivery_report,
+    )
     producer.flush()
-    print("Sent:", user)
     time.sleep(2)
