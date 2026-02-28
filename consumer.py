cfc10106f9da18dadbfbbb81b294d0eeacccca51
Ahmed <ahmedelsharawy117@gmail.com>
2026-02-28 23:37:46 +0200

    feat: add avro consumer with schema fetching

--- consumer.py
+++ consumer.py
@@ -1,6 +1,21 @@
 import json
-
 from confluent_kafka import Consumer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroDeserializer
+from confluent_kafka.serialization import MessageField, SerializationContext
+
+
+def dict_to_user(obj, ctx):
+    return obj
+
+
+with open("user.avsc", "r") as f:
+    schema_str = f.read()
+
+schema_registry_conf = {"url": "http://localhost:8081"}
+schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+avro_deserializer = AvroDeserializer(schema_registry_client, schema_str, dict_to_user)
 
 consumer_conf = {
     "bootstrap.servers": "localhost:9092",
@@ -8,7 +23,7 @@ consumer_conf = {
     "auto.offset.reset": "earliest",
 }
 
-consumer = Consumer(consumer_conf)
+consumer = Consumer(dict(consumer_conf))
 
 consumer.subscribe(["users"])
 
@@ -17,15 +32,20 @@ print("🟢 Consumer is running and subscribed to users topic")
 try:
     while True:
         msg = consumer.poll(1.0)
+
         if msg is None:
             continue
+
         if msg.error():
             print("❌ Error:", msg.error())
             continue
 
-        value = msg.value().decode("utf-8")
-        user = json.loads(value)
-        print(user)
+        user = avro_deserializer(
+            msg.value(), SerializationContext(msg.topic(), MessageField.VALUE)
+        )
+
+        j = json.dumps(user, indent=4)
+        print(j)
 except KeyboardInterrupt:
     print("\n🔴 Stopping consumer")
 
